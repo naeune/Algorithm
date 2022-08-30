3185bf516fe658eacf143398e6cb33134bc70a28
naeune <ivaeune@gmail.com>
2022-08-30 23:35:00 +0900

    1001번

---
+++ src/algorithm/Al_1001.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Al_1001 {
+    public static void main(String[] args) {
+        
+        int A = 0;
+        int B = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        A = sc.nextInt();
+        B = sc.nextInt();
+        
+        System.out.println(A-B);
+        
+        sc.close();
+    }
+}
