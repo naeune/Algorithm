5c88c50657ec1011c07e2a1b64c524a92613622a
naeune <ivaeune@gmail.com>
2022-08-31 15:59:43 +0900

    백준 알고리즘 7번까

---
+++ src/level1/Al_1008.java
@@ -0,0 +1,21 @@
+package level1;
+
+import java.util.Scanner;
+
+public class Al_1008 {
+	public static void main(String[] args) {
+		
+        double A = 0;
+        double B = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        A = sc.nextInt();
+        B = sc.nextInt();
+        
+        System.out.println(A/B);
+        
+        sc.close();
+        
+		}
+}
---
+++ src/level1/Al_10869.java
@@ -0,0 +1,23 @@
+package level1;
+
+import java.util.Scanner;
+
+public class Al_10869 {
+	public static void main(String[] args) {
+		int A = 0;
+        int B = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        A = sc.nextInt();
+        B = sc.nextInt();
+        
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+        
+        sc.close();
+		}
+}
